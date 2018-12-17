16476ed6f7bc1a1122c022d098dc0e19589c6caf
yottanami <yottanami@gmail.com>
2018-12-17 19:23:11 +0330

    Install devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+
+    # Workaround for --API mode
+    # rawgit is used to work around github's use of text/plain MIME types'
+
+    get "/javascripts/graphiql/rails/application.js", to: redirect('/javascripts/graphiql.js')
+    get "/stylesheets/graphiql/rails/application.css", to: redirect('/stylesheets/graphiql.css')
+  end
 end
