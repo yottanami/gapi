7c2a560f528c1b52d49f6b67a0019398fb7885fb
yottanami <yottanami@gmail.com>
2019-04-29 17:50:45 +0430

    Add authentication to graphql controller

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -20,7 +20,7 @@ class GraphqlController < ApplicationController
 
     context = {
       # Query context goes here, for example:
-      # current_user: current_user || User.last,
+       current_user: current_user,
     }
     result = GapiSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
