0fd978b246b9f7d76eaa7284d295de54dfde49b9
yottanami <yottanami@gmail.com>
2019-05-18 16:52:56 +0430

    Modify model collector

--- lib/model_collector.rb
+++ lib/model_collector.rb
@@ -6,4 +6,20 @@ module ModelCollector
     ApplicationRecord.descendants
   end
 
+  def self.belongs_tos model
+    #model.reflect_on_all_associations(:has_many)
+    model.reflect_on_all_associations(:belongs_to).map(&:name)
+  end
+
+  def self.belongs_tos model
+    model.reflect_on_all_associations(:has_many).map(&:name)
+  end
+
+  def test_cat
+    Types.const_set("CategoryType",
+      Class.new(Types::BaseObject)  do
+        field :id, Types::BaseObject::ID, null: false
+      end
+    )
+  end
 end
